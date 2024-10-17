96f5a87682cd3e718950a4fd7b967c2b4458d726
Violet Chaffee <26vchaffee@castilleja.org>
2024-10-17 10:50:53 -0700

    item adding is wokring now!

---
+++ CollectIndex/ItemRow.swift
@@ -0,0 +1,18 @@
+//
+//  ItemRow.swift
+//  CollectIndex
+//
+//  Created by Violet Chaffee on 10/15/24.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemRow()
+}
---
+++ CollectIndex/NewItemView.swift
@@ -0,0 +1,18 @@
+//
+//  NewItemView.swift
+//  CollectIndex
+//
+//  Created by Violet Chaffee on 10/15/24.
+//
+
+import SwiftUI
+
+struct NewItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewItemView()
+}
