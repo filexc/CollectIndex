15e7eb5e9bf55dce3178937131de84586d2d98fd
Violet Chaffee <26vchaffee@castilleja.org>
2024-12-02 09:30:30 -0800

    final changes

--- CollectIndex/CollectionView.swift
+++ CollectIndex/CollectionView.swift
@@ -17,97 +17,100 @@ struct CollectionView: View {
 
     var body: some View {
         NavigationStack {
-            //this may be bad -- force unwrapping
-            ForEach(
-                collections.collectionArray[
-                    collections.collectionArray.firstIndex(where: {
-                        $0.id == collection.id
-                    })!
-                ].items
-            ) { item in
-                VStack {
-                    NavigationLink {
-                        ItemView(item: item)
-                    } label: {
-                        HStack {
-                            Image(uiImage: UIImage(data: item.image.photo)!)
-                                .resizable()
-                                .frame(width: 60, height: 60)
-                                .clipShape(
-                                    RoundedRectangle(
-                                        cornerSize: CGSize(
-                                            width: 10, height: 10)))
-                            Text(item.name)
-                                .font(.custom(settings.fontChoice, size: 24))
-                                .foregroundStyle(Color(settings.textColor))
-                            Spacer()
-                            Button {
-                                itemToDelete = item
-                                showingDeleteAlert = true
-                            } label: {
-                                Image(systemName: "trash")
-                                    .foregroundStyle(.red)
-                            }
-                            .alert(isPresented: $showingDeleteAlert) {
-                                Alert(
-                                    title: Text("Confirm Delete"),
-                                    message: Text(
-                                        "You cannot undo this action"),
-                                    primaryButton: .destructive(Text("Delete"))
-                                    {
-                                        if let item = itemToDelete {
-                                            deleteItem(item: item)
+            ScrollView{
+                //this may be bad -- force unwrapping
+                ForEach(
+                    collections.collectionArray[
+                        collections.collectionArray.firstIndex(where: {
+                            $0.id == collection.id
+                        })!
+                    ].items
+                ) { item in
+                    VStack {
+                        NavigationLink {
+                            ItemView(item: item)
+                        } label: {
+                            HStack {
+                                Image(uiImage: UIImage(data: item.image.photo)!)
+                                    .resizable()
+                                    .frame(width: 60, height: 60)
+                                    .clipShape(
+                                        RoundedRectangle(
+                                            cornerSize: CGSize(
+                                                width: 10, height: 10)))
+                                Text(item.name)
+                                    .font(.custom(settings.fontChoice, size: 24))
+                                    .foregroundStyle(Color(settings.textColor))
+                                Spacer()
+                                Button {
+                                    itemToDelete = item
+                                    showingDeleteAlert = true
+                                } label: {
+                                    Image(systemName: "trash")
+                                        .foregroundStyle(.red)
+                                }
+                                .alert(isPresented: $showingDeleteAlert) {
+                                    Alert(
+                                        title: Text("Confirm Delete"),
+                                        message: Text(
+                                            "You cannot undo this action"),
+                                        primaryButton: .destructive(Text("Delete"))
+                                        {
+                                            if let item = itemToDelete {
+                                                deleteItem(item: item)
+                                            }
+                                            itemToDelete = nil
+                                        },
+                                        secondaryButton: .cancel {
+                                            itemToDelete = nil
                                         }
-                                        itemToDelete = nil
-                                    },
-                                    secondaryButton: .cancel {
-                                        itemToDelete = nil
-                                    }
-                                )
+                                    )
+                                }
+                            }
+                            .padding()
+                        }
+                    }
+                    .frame(width: 320, height: 80, alignment: .topLeading)
+                    .foregroundColor(Color(settings.textColor))
+                }
+                NavigationLink {
+                    NewItemView(collection: collection)
+                } label: {
+                    HStack {
+                        Image(systemName: "plus.square")
+                            .font(.system(size: 65))
+                            .frame(width: 65, height: 65, alignment: .topLeading)
+                            .foregroundStyle(Color(settings.textColor))
+                        Text("New Item")
+                            .font(.custom(settings.fontChoice, size:24))
+                            .foregroundStyle(Color(settings.textColor))
+                        Spacer()
+                    }
+                }
+                .padding(.leading, 50)
+                Spacer()
+                    .toolbar {
+                        ToolbarItem(placement: .principal) {
+                            VStack {
+                                Text("Collection: " + collection.name)
+                                    .font(
+                                        .custom(
+                                            settings.fontChoice, size: 16,
+                                            relativeTo: .headline)
+                                    )
+                                    .bold()
+                                    .foregroundStyle(Color(settings.textColor))
                             }
                         }
-                        .padding()
                     }
-                }
-                .frame(width: 320, height: 80, alignment: .topLeading)
-                .foregroundColor(Color(settings.textColor))
+                    .navigationDestination(for: Item.self) { item in
+                        ItemView(item: item)
+                    }
             }
-            NavigationLink {
-                NewItemView(collection: collection)
-            } label: {
-                HStack {
-                    Image(systemName: "plus.square")
-                        .font(.system(size: 65))
-                        .frame(width: 65, height: 65, alignment: .topLeading)
-                        .foregroundStyle(Color(settings.textColor))
-                    Text("New Item")
-                        .font(.custom(settings.fontChoice, size:24))
-                        .foregroundStyle(Color(settings.textColor))
-                    Spacer()
-                    }
-                }
-            .padding(.leading, 50)
-            Spacer()
-                .toolbar {
-                    ToolbarItem(placement: .principal) {
-                        VStack {
-                            Text("Collection: " + collection.name)
-                                .font(
-                                    .custom(
-                                        settings.fontChoice, size: 16,
-                                        relativeTo: .headline)
-                                )
-                                .bold()
-                                .foregroundStyle(Color(settings.textColor))
-                        }
-                    }
-                }
-                .navigationDestination(for: Item.self) { item in
-                    ItemView(item: item)
-                }
         }
         .containerRelativeFrame([.horizontal, .vertical])
         .background(Color(settings.backgroundColor))
+        .toolbarBackground(Color(settings.backgroundColor))
     }
 
     func deleteItem(item: Item) {
